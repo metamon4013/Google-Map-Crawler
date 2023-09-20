0167fbf03a2e7cf22e6af6eb88ad935495e8f0bf
metamon4013 <satoshi.noguchi4013@gmail.com>
2023-09-20 20:44:32 +0900

    First Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      api_key: process.env.API_KEY
+    }
+  }
+})
